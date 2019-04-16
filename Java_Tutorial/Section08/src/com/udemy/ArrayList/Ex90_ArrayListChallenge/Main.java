3250b33a5d7400a0b193d1a703bceb1365b42f35
Aidas <aidas.pupelis@gmail.com>
2019-04-16 19:42:53 +0300

    second commit

--- Java_Tutorial/Section08/src/com/udemy/ArrayList/Ex90_ArrayListChallenge/Main.java
+++ Java_Tutorial/Section08/src/com/udemy/ArrayList/Ex90_ArrayListChallenge/Main.java
@@ -2,16 +2,16 @@ package com.udemy.ArrayList.Ex90_ArrayListChallenge;
 
 // Create a program that implements a simple mobile phone with the following capabilities.
 // Able to store, modify, remove and query contact names.
-// You will want to create a separate class for MobilePhone (name and phone number).
-// Create a master class (Main) that holds the ArrayList of MobilePhone
-// The Main class has the functionality listed above.
+// You will want to create a separate class for Contacts (name and phone number).
+// Create a master class (MobilePhone) that holds the ArrayList of Contacts
+// The MobilePhone class has the functionality listed above.
 // Add a menu of options that are available.
 // Options:  Quit, print list of contacts, add new contact, update existing contact, remove contact
 // and search/find contact.
 // When adding or updating be sure to check if the contact already exists (use name)
-// Be sure not to expose the inner workings of the Arraylist to Main
+// Be sure not to expose the inner workings of the Arraylist to MobilePhone
 // e.g. no ints, no .get(i) etc
-// Main should do everything with Contact objects only.
+// MobilePhone should do everything with Contact objects only.
 
 import java.util.ArrayList;
 import java.util.Scanner;
---
+++ Java_Tutorial/Section08/src/com/udemy/LinkedArrayList/Ex101_LinkedArrayChallenge/Album.java
@@ -0,0 +1,71 @@
+package com.udemy.LinkedArrayList.Ex101_LinkedArrayChallenge;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class Album {
+    private String albumName;
+    private String performerName;
+    private ArrayList<Song> songs;
+
+
+    public Album(String albumName, String performerName) {
+        this.albumName = albumName;
+        this.performerName = performerName;
+        this.songs = new ArrayList<>();
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+
+    public void addNewSongToAlbum(String songName, double songDuration) {
+        if (checkIfSongExist(songName)) {
+            System.out.println("This song already included in album");
+        } else {
+            Song song = new Song(songName, songDuration);
+            this.songs.add(song);
+        }
+    }
+
+    public void addSongToPlaylist(String songName, LinkedList<Song> playlist) {
+        Song existingSong = findSongByName(songName);
+        if(findSongByName(songName) != null) {
+            playlist.add(existingSong);
+        } else {
+            System.out.println("Song " + songName + " can't be added to playlist (it isn't included in album)");
+        }
+    }
+
+    /// helpful methods
+
+    private boolean checkIfSongExist(String songName) {
+        for (Song checkedSong: this.songs) {
+            if (checkedSong.getName().equals(songName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Song findSongByName(String songName) {
+        for (Song findedSong: this.songs) {
+            if(findedSong.getName().equals(songName)) {
+                return findedSong ;
+            }
+        }
+        return null;
+    }
+
+    public void printSongsInAlbum() {
+        System.out.println("Album " + this.albumName + " contains " + this.songs.size() + " songs:");
+        for (int i = 0; i < this.songs.size(); i++) {
+            System.out.println("Song Nr." + (i + 1) + " " + this.songs.get(i).getName() + ", duration: " + this.songs.get(i).getDuration());
+        }
+        System.out.println();
+    }
+
+
+}
