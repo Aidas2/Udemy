3da066ad4b2df323b86d50597b9c3942ab4deb76
Aidas Pupelis <aidas.pupelis@metasite.net>
2019-04-18 15:38:10 +0300

    Angular tutorial completed

--- Angular/angular-tour-of-heroes/src/app/mock-heroes.ts
+++ Angular/angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -1,5 +1,7 @@
 import {Hero} from './hero';
 
+//this class already not necessary, may be deleted;
+
 export const HEROES: Hero[] = [
     { id: 11, name: 'Mr. Nice' },
     { id: 12, name: 'Narco' },
