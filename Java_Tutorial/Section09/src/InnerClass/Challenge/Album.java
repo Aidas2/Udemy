a8c647fca56f53fdf51c46f255401355a4ebce42
Aidas <aidas.pupelis@gmail.com>
2019-04-26 07:11:55 +0300

    chall

--- Java_Tutorial/Section09/src/InnerClass/Challenge/Album.java
+++ Java_Tutorial/Section09/src/InnerClass/Challenge/Album.java
@@ -18,43 +18,56 @@ import java.util.LinkedList;
 public class Album {
     private String name;
     private String artist;
-    private ArrayList<Song> songs;
+    private SongList songs;
 
     public Album(String name, String artist) {
         this.name = name;
         this.artist = artist;
-        this.songs = new ArrayList<Song>();
+        this.songs = new SongList();
     }
 
+//    public boolean addSong(String title, double duration) {
+//        if(findSong(title) == null) {
+//            this.songs.add(new Song(title, duration));
+//            return true;
+//        }
+//        return false;
+//    }
     public boolean addSong(String title, double duration) {
-        if(findSong(title) == null) {
-            this.songs.add(new Song(title, duration));
-            return true;
-        }
-        return false;
+        return this.songs.addSong(new Song(title, duration));
     }
 
-    private Song findSong(String title) {
-        for(Song checkedSong: this.songs) {
-            if(checkedSong.getTitle().equals(title)) {
-                return checkedSong;
-            }
-        }
-        return null;
-    }
-
+//    public boolean addToPlayList(int trackNumber, LinkedList<Song> playList) {
+//        int index = trackNumber -1;
+//        if((index >=0) && (index <= this.songs.size())) {
+//            playList.add(this.songs.get(index));
+//            return true;
+//        }
+//        System.out.println("This album does not have a track " + trackNumber);
+//        return false;
+//    }
     public boolean addToPlayList(int trackNumber, LinkedList<Song> playList) {
-        int index = trackNumber -1;
-        if((index >=0) && (index <= this.songs.size())) {
-            playList.add(this.songs.get(index));
+        Song checkedSong = this.songs.findSong(trackNumber);
+        if(checkedSong != null) {
+            playList.add(checkedSong);
             return true;
+        } else {
+            System.out.println("This album does not have a track " + trackNumber);
+            return false;
         }
-        System.out.println("This album does not have a track " + trackNumber);
-        return false;
     }
 
+//    public boolean addToPlayList(String title, LinkedList<Song> playList) {
+//        Song checkedSong = findSong(title);
+//        if(checkedSong != null) {
+//            playList.add(checkedSong);
+//            return true;
+//        }
+//        System.out.println("The song " + title + " is not in this album");
+//        return false;
+//    }
     public boolean addToPlayList(String title, LinkedList<Song> playList) {
-        Song checkedSong = findSong(title);
+        Song checkedSong = this.songs.findSong(title);
         if(checkedSong != null) {
             playList.add(checkedSong);
             return true;
@@ -63,21 +76,40 @@ public class Album {
         return false;
     }
 
+//=========================================================
 
     private class SongList {
         private ArrayList<Song> songs;
 
-        public SongList(ArrayList<Song> songs) {
-            this.songs = songs;
+        public SongList() {
+            this.songs = new ArrayList<>();
         }
 
-        public void addSong(String title, double duration) {
-
+        public boolean addSong(Song song) {
+            if(this.songs.contains(song)) {
+                return false;
+            } else {
+                this.songs.add(song);
+                return true;
+            }
         }
 
-        public Song findSong (String title) {
-
+        private Song findSong(String title) {
+            for(Song checkedSong: this.songs) {
+                if(checkedSong.getTitle().equals(title)) {
+                    return checkedSong;
+                }
+            }
             return null;
+        }
+
+        public Song findSong (int trackNumber) {
+            int index = trackNumber - 1;
+            if(index >= 0 && index < this.songs.size()){
+                return this.songs.get(index);
+            } else {
+                return null;
+            }
         }
     }
 
