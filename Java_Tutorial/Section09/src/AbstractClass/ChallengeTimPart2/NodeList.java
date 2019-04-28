f5b57f55333fb6ec94519334051e53c87f11cd3c
Aidas <aidas.pupelis@gmail.com>
2019-04-28 19:37:02 +0300

    09

---
+++ Java_Tutorial/Section09/src/AbstractClass/ChallengeTimPart2/NodeList.java
@@ -0,0 +1,11 @@
+package AbstractClass.ChallengeTimPart2;
+
+/**
+ * Created by dev on 11/10/2015.
+ */
+public interface NodeList {
+    ListItem getRoot();
+    boolean addItem(ListItem item);
+    boolean removeItem(ListItem item);
+    void traverse(ListItem root);
+}
---
+++ Java_Tutorial/Section09/src/AbstractClass/Example/CanFly.java
@@ -0,0 +1,8 @@
+package AbstractClass.Example;
+
+/**
+ * Created by dev on 9/10/2015.
+ */
+public interface CanFly {
+    void fly();
+}
