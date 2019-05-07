7097e138d23594a97c970ca4ae259c756ab474d5
Aidas Pupelis <aidas.pupelis@metasite.net>
2019-05-07 14:47:22 +0300

    MPD repeating

--- Java_Tutorial/Section10/src/com/udemy/section12/challengeAidas/Seat.java
+++ Java_Tutorial/Section10/src/com/udemy/section12/challengeAidas/Seat.java
@@ -1,16 +1,55 @@
 package com.udemy.section12.challengeAidas;
 
-public class Seat {
+import java.util.Comparator;
+
+public class Seat implements Comparable<Seat> {
     private final String seatNumber;
-    public boolean reserved = false;
+    private double price;
+    private boolean reserved = false;
+
 
     public Seat(String seatNumber) {
         this.seatNumber = seatNumber;
-    }
+    } // Lesson 135
+
+    public Seat(String seatNumber, double price) {
+        this.seatNumber = seatNumber;
+        this.price = price;
+    } //Lesson 138
 
     public String getSeatNumber() {
         return seatNumber;
     }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+        @Override //comparator for Lesson 136:
+    public int compareTo(Seat seat) {
+        return this.seatNumber.compareToIgnoreCase(seat.getSeatNumber());
+    }
+
+    //comparator for Lesson 138
+    static final Comparator<Seat> PRICE_ORDER = new Comparator<Seat>() {
+        @Override
+        public int compare(Seat seat1, Seat seat2) {
+            if(seat1.getPrice() < seat2.getPrice()) {
+                return -1;
+
+            } else if ((seat1.getPrice() > seat2.getPrice())) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    };
+
+
+
 
     public boolean reserve() {
         if (!this.reserved) {
@@ -31,4 +70,6 @@ public class Seat {
             return false;
         }
     }
+
+
 }
--- Java_Tutorial/Section10/src/com/udemy/section12/challengeAidas/Theatre.java
+++ Java_Tutorial/Section10/src/com/udemy/section12/challengeAidas/Theatre.java
@@ -2,6 +2,8 @@ package com.udemy.section12.challengeAidas;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
@@ -11,7 +13,8 @@ public class Theatre {
     private  String theatreName;
     private  int numberOfRows;
     private  int seatPerRow;
-    private Collection<Seat> seats = new LinkedHashSet<>();
+    //public List<Seat> seats = new ArrayList<>(); //just for Lesson 137
+    private List<Seat> seats = new ArrayList<>();
 
     public Theatre(String theatreName, int numberOfRows, int seatPerRow) {
         this.theatreName = theatreName;
@@ -22,7 +25,17 @@ public class Theatre {
         //int lastRow = 65 + (numberOfRows - 1); // firstRow 'A' = 65;
         for (char row = 'A'; row <= lastRow; row++) {
             for(int seatNumber = 1; seatNumber <= seatPerRow; seatNumber++) {
-                Seat seat = new Seat(row + String.format("%02d", seatNumber));
+
+                //Lesson 138:
+                double price = 12.00;
+                if (row < 'D' && (seatNumber >= 4 && seatNumber <= 16)) {
+                    price = 14.00;
+                } else if (row > 'F' || (seatNumber >= 4 && seatNumber <= 16)){
+                    price = 7.00;
+                }
+
+                //Seat seat = new Seat(row + String.format("%02d", seatNumber));
+                Seat seat = new Seat(row + String.format("%02d", seatNumber), price); //Lesson 138
                 seats.add(seat);
             }
         }
@@ -34,29 +47,47 @@ public class Theatre {
     }
 
     public boolean reserveSeat (String seatNumber) {
-        Seat requestedSeat = null;
-        for(Seat s: seats) {
-            if(s.getSeatNumber().equals(seatNumber)) {
-                requestedSeat = s;
-                break;
-            }
-        }
+        //Lesson 135:
+//        Seat requestedSeat = null;
+//        for(Seat s: seats) {
+//            System.out.print(".");
+//            if(s.getSeatNumber().equals(seatNumber)) {
+//                requestedSeat = s;
+//                break;
+//            }
+//        }
+//        if(requestedSeat == null) {
+//            System.out.println("There is no seat " + seatNumber);
+//            return false;
+//        }
+        //return true;
+//        return requestedSeat.reserve();
 
-        if(requestedSeat == null) {
+        //Lesson 136:
+        Seat requestedSeat = new Seat(seatNumber);
+        int foundSeat = Collections.binarySearch(seats, requestedSeat, null);
+        if (foundSeat >= 0) {
+            return seats.get(foundSeat).reserve();
+        } else {
             System.out.println("There is no seat " + seatNumber);
             return false;
         }
-        //return true;
-        return requestedSeat.reserve();
     }
 
-    public void getSeats() {
-        System.out.println("There are " + seats.size() + " seats in " + theatreName + " theatre: ");
-        for(Seat s: seats) {
-            System.out.println(s.getSeatNumber());
-        }
-    }
+    //Lesson 135
+    // for testing (because Seat class is private)
+//    public void getSeats() {
+//        System.out.println("There are " + seats.size() + " seats in " + theatreName + " theatre: ");
+//        for(Seat s: seats) {
+//            System.out.println(s.getSeatNumber());
+//        }
+//    }
 
+    //Lesson 138
+    public Collection<Seat> getSeats() {
+        return seats;
+
+    }
 
 }
 
