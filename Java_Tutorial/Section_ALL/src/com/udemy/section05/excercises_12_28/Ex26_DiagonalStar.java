d74dbdde66085f10cb370dfb27ddeb0a082b8a44
Aidas <aidas.pupelis@gmail.com>
2019-07-06 14:33:37 +0300

    all

---
+++ Java_Tutorial/Section_ALL/src/com/udemy/section05/excercises_12_28/Ex26_DiagonalStar.java
@@ -0,0 +1,59 @@
+package com.udemy.section05.excercises_12_28;
+
+public class Ex26_DiagonalStar {
+
+    public static void main(String[] args) {
+        printSquareStar(7);
+        System.out.println("=====================");
+        printSquareStar2(7);
+
+    }
+
+    // i is for rows; j is for columns;
+    //System.out.println() must be after second for but inside first for
+
+    public static void printSquareStar (int number) {
+        if (number < 5 ) {
+            System.out.println("Invalid Value");
+        } else {
+
+
+            for (int i = 0; i < number; i++) {
+                for (int j = 0; j < number; j++) {
+                    if (i == 0 || i == number-1 || j == 0 || j == number-1 || i == j || j == number - i - 1 ) {
+                        System.out.print("*  ");
+                    } else {
+                        System.out.print("   ");
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
+
+
+    //==================================== example01 from Udemy.com
+    public static void printSquareStar2(int number){
+        if(number<5){
+            System.out.println("Invalid Value");
+        } else {
+            for(int i= 1; i<= number; i++){
+                for(int j=1; j<= number;j++){
+                    if(i==1 || j==1 || i==j || i==number || j==number || i== number-j+1) {
+                        System.out.print("* ");
+                    } else {
+                        System.out.print("  ");
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
+
+
+
+
+}
+
+
+
---
+++ Java_Tutorial/Section_ALL/src/com/udemy/section08/arrays/Array/Ex86_ReverseArrayChallenge.java
@@ -0,0 +1,41 @@
+package com.udemy.section08.arrays.Array;
+
+import java.util.Arrays;
+
+/*
+-Write a method called reverse() with an int array as a parameter.
+
+-The method should not return any value. In other words, the method is allowed to modify the array parameter.
+
+-In main() test the reverse() method and print the array both reversed and non-reversed.
+
+-To reverse the array, you have to swap the elements, so that the first element is swapped with the last element and so on.
+
+-For example01, if the array is {1, 2, 3, 4, 5}, then the reversed array is {5, 4, 3, 2, 1}.
+
+Tip:
+	-Create a new console project with the name eReverseArrayChallengef
+ */
+
+public class Ex86_ReverseArrayChallenge {
+    public static void main(String[] args) {
+
+        int[] myArray = new int[] {1,2,3,4,5,6,7};
+        System.out.println("Array before reverse: " + Arrays.toString(myArray));
+        reverse(myArray);
+        System.out.println("Array after reverse: " + Arrays.toString(myArray));
+    }
+
+
+    public static void reverse(int[] array) {
+
+        int halfLength = array.length / 2;
+
+        for (int i = 0; i < halfLength; i++) {
+            int temp = array[i];
+            array[i] = array[array.length-1-i];
+            array[array.length-1-i] = temp;
+            //System.out.println("index i="+i + ": " + Arrays.toString(array));
+        }
+    }
+}
