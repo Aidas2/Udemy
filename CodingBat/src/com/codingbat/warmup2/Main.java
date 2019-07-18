10716156397d53d3a98c161fb33274f6612ee2ab
Aidas <aidas.pupelis@gmail.com>
2019-07-18 21:23:05 +0300

    codingbat

--- CodingBat/src/com/codingbat/warmup2/Main.java
+++ CodingBat/src/com/codingbat/warmup2/Main.java
@@ -8,9 +8,10 @@ public class Main {
         countXX("abcxx");
         doubleX("axxbb");
         doubleXX("axxbb");
+        stringBits("Heeololeo");
     }
 
-//    Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+    //    Given a string and a non-negative int n, return a larger string that is n copies of the original string.
 //    stringTimes("Hi", 2) → "HiHi"
 //    stringTimes("Hi", 3) → "HiHiHi"
 //    stringTimes("Hi", 1) → "Hi"
@@ -23,7 +24,7 @@ public class Main {
         return newStr;
     }
 
-//    Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars,
+    //    Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars,
 //    or whatever is there if the string is less than length 3. Return n copies of the front;
 //    frontTimes("Chocolate", 2) → "ChoCho"
 //    frontTimes("Chocolate", 3) → "ChoChoCho"
@@ -43,7 +44,7 @@ public class Main {
         return newStr;
     }
 
-//    Count the number of "xx" in the given string. We'll say that overlapping is allowed, so "xxx" contains 2 "xx".
+    //    Count the number of "xx" in the given string. We'll say that overlapping is allowed, so "xxx" contains 2 "xx".
 //    countXX("abcxx") → 1
 //    countXX("xxx") → 2
 //    countXX("xxxx") → 3
@@ -59,7 +60,7 @@ public class Main {
         return count;
     }
 
-//    Given a string, return true if the first instance of "x" in the string is immediately followed by another "x".
+    //    Given a string, return true if the first instance of "x" in the string is immediately followed by another "x".
 //    doubleX("axxbb") → true
 //    doubleX("axaxax") → false
 //    doubleX("xxxxx") → true
@@ -69,9 +70,9 @@ public class Main {
         boolean doubleX = true;
 
         for (int i = 0; i < str.length() - 1; i++) {
-            if(str.charAt(i) == 'x') {
+            if (str.charAt(i) == 'x') {
                 firstX = str.charAt(i);
-                afterX = str.charAt(i+1);
+                afterX = str.charAt(i + 1);
                 System.out.println("\n" + "\n" + "cikle " + firstX + " : " + afterX);
                 break;
             }
@@ -91,10 +92,10 @@ public class Main {
     public static boolean doubleXX(String str) {
         int firstXPosition = str.indexOf("x");
         if (firstXPosition == -1) return false; // no "x" at all
-        if (firstXPosition+1 >= str.length()) return false; // check i+1 in bounds?
+        if (firstXPosition + 1 >= str.length()) return false; // check i+1 in bounds?
 
         char firstX = str.charAt(firstXPosition);
-        char afterX = str.charAt(firstXPosition+1);
+        char afterX = str.charAt(firstXPosition + 1);
         System.out.println("firstX = " + firstX + ", afterX = " + afterX);
         return firstX == afterX;
 
@@ -102,4 +103,33 @@ public class Main {
         // String x = str.substring(i);
         // return x.startsWith("xx");
     }
+
+
+//    Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
+//    stringBits("Hello") → "Hlo"
+//    stringBits("Hi") → "H"
+//    stringBits("Heeololeo") → "Hello"
+
+    public static String stringBits(String str) {
+        String result = "";
+
+//        for (int i = 0; i < str.length(); i++) {
+//            if (i % 2 == 0) {
+//                result = result + str.charAt(i);
+//                System.out.println("i = " + i + ", char = " + str.charAt(i));
+//            }
+//        }
+
+        //alternative solution:
+        for (int i = 0; i < str.length(); i += 2) {
+            result += str.charAt(i);
+            System.out.println("i = " + i + ", char = " + str.charAt(i));
+        }
+
+        System.out.println("Result: " + result);
+        return result;
+
+
+    }
+
 }
