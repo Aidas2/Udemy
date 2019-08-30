a66c7b5cf3fbbf99cb295d714ed35465e5640a6a
Aidas Pupelis <aidas.pupelis@asseco.lt>
2019-08-30 15:41:47 +0300

    JS 51

--- Javascript/4-DOM-pig-game/starter/app.js
+++ Javascript/4-DOM-pig-game/starter/app.js
@@ -8,3 +8,83 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    
+    //1. Generate random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    //2. Display the result (as picture)
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    //3. update tehe round score IF the rolled number was NOT a 1
+    if(dice !== 1) {
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        //next player
+        nextPlayer();
+    }
+
+
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    //1. add CURRENT score to clobal score
+    scores[activePlayer] += roundScore;
+    //2. update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    
+    //3. check if player won a game
+    if(scores[activePlayer] >= 10) {
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+
+    } else {
+        //next player
+        nextPlayer();
+    }
+
+
+
+});
+
+function nextPlayer() {
+    //next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    // document.querySelector('.player-0-panel').classList.remove('active');
+    // document.querySelector('.player-1-panel').classList.add('active');
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
+}
+
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+//var x = document.querySelector('#current-0').textContent;
+//console.log(x);
