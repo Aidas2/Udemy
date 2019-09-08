d484f84f721e5a56b4979da8170ff974b5343bb5
Aidas <aidas.pupelis@gmail.com>
2019-09-08 21:24:50 +0300

    webpack

--- Javascript/9-forkify/starter/src/js/index.js
+++ Javascript/9-forkify/starter/src/js/index.js
@@ -1,1 +1,32 @@
 // Global app controller
+import num from './test';
+
+const x = 23;
+
+// console.log(`I imported ${num} from another module`)
+console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`);
+
+/**
+ * npm install webpack --save-dev
+ * npm install webpack-cli --save-dev
+ * 
+ * npm run dev
+ * npm run build
+ * 
+ * npm install webpack-dev-server --save-dev
+ * npm run start
+ * 
+ * npm install html-webpack-plugin --save-dev
+ * npm run start
+ * npm run dev
+ * 
+ * npm install babel-core babel-preset-env babel-loader --save-dev
+ * npm install babel-polyfill --save-dev
+ * npm run dev
+ * 
+ * 
+ * //just examples:
+ * npm install jquery --save        //install (localy)
+ * npm uninstall jquery --save      //uninstall
+ * sudo npm install live-server --global    //instal (globaly)
+ */
