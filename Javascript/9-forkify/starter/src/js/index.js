bd32941e7094d580d7fdac16c2b616b1efff6fa1
Aidas Pupelis <aidas.pupelis@asseco.lt>
2019-09-09 19:56:42 +0300

    JS forkify

--- Javascript/9-forkify/starter/src/js/index.js
+++ Javascript/9-forkify/starter/src/js/index.js
@@ -1,32 +1,13 @@
 // Global app controller
+/*
 import num from './test';
-
 const x = 23;
-
 // console.log(`I imported ${num} from another module`)
 console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`);
+*/
 
-/**
- * npm install webpack --save-dev
- * npm install webpack-cli --save-dev
- * 
- * npm run dev
- * npm run build
- * 
- * npm install webpack-dev-server --save-dev
- * npm run start
- * 
- * npm install html-webpack-plugin --save-dev
- * npm run start
- * npm run dev
- * 
- * npm install babel-core babel-preset-env babel-loader --save-dev
- * npm install babel-polyfill --save-dev
- * npm run dev
- * 
- * 
- * //just examples:
- * npm install jquery --save        //install (localy)
- * npm uninstall jquery --save      //uninstall
- * sudo npm install live-server --global    //instal (globaly)
- */
+import str from './models/Search';
+// import { add as a, multiply as m, ID } from './views/searchView';
+import * from './views/searchView';
+
+console.log(`Using imported functions! ${a(ID, 2)} and ${m(3, 5)}. ${str}`);
