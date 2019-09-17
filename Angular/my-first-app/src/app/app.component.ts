ec6e2be0b7c868012f3f21adde58dfb3848c1efb
Aidas Pupelis <aidas.pupelis@asseco.lt>
2019-09-17 16:49:28 +0300

    Angular 01

--- Angular/my-first-app/src/app/app.component.ts
+++ Angular/my-first-app/src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [`
+  h3 {
+    color: dodgerblue;
+  }
+  `]
+
 })
 export class AppComponent {
-  title = 'my-first-app';
+  name = 'Aidas';
 }
