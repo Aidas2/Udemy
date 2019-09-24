7ba774a2a17f3c3380750a16831f1ef76aa2c2f3
Aidas Pupelis <aidas.pupelis@asseco.lt>
2019-09-24 17:28:02 +0300

    Angular shoppingListf

--- Angular/ng4-complete-guide/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ Angular/ng4-complete-guide/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, EventEmitter, Output } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,11 @@ import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
-
   @ViewChild('nameInput'/*, { static: false }*/) nameInputRef: ElementRef;
   @ViewChild('amountInput'/*, { static: false }*/) amountInputRef: ElementRef;
+
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
 
   constructor() { }
 
@@ -17,6 +20,9 @@ export class ShoppingEditComponent implements OnInit {
   }
 
   onAddItem() {
-
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.ingredientAdded.emit(newIngredient);
   }
 }
--- Angular/ng4-complete-guide/src/app/shopping-list/shopping-list.component.ts
+++ Angular/ng4-complete-guide/src/app/shopping-list/shopping-list.component.ts
@@ -17,4 +17,9 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit() {
   }
 
+
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
+
 }
