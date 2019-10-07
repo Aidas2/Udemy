6c5e29f1042231b8ad2daf3dc66222a6ebebf294
Aidas Pupelis <aidas.pupelis@asseco.lt>
2019-10-07 17:05:50 +0300

    Angular forms

--- Angular/07_forms/src/app/app.component.ts
+++ Angular/07_forms/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f', { static: true }) signupForm: NgForm;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
+  // alternative version:
+  onSubmit() {
+    console.log(this.signupForm);
   }
 }
--- Angular/07_forms/src/app/app.module.ts
+++ Angular/07_forms/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+// import { HttpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
+    // HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
