a4941cf7e9723ddb7d5d8c5c66ecfff4606ff59f
Aidas Pupelis <aidas.pupelis@asseco.lt>
2019-10-23 17:22:12 +0300

    Angular http

--- Angular/09_http/src/app/app.component.ts
+++ Angular/09_http/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Subscription } from 'rxjs';
+
 import { Post } from './post.model';
 import { PostsService } from './posts.service';
 
@@ -8,18 +10,32 @@ import { PostsService } from './posts.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loadedPosts: Post[] = [];
   isFetching = false;
+  error = null;    // for using when subscribing in app.component.ts
+  private errorSub: Subscription;
 
   constructor(private http: HttpClient, private postsService: PostsService) { }
 
   ngOnInit() {
-    this.isFetching = true;
-    this.postsService.fetchPosts().subscribe(posts => {
-      this.isFetching = false;
-      this.loadedPosts = posts;
+
+    this.errorSub = this.postsService.error.subscribe(errorMessage => {
+      this.error = errorMessage;
     });
+
+
+    this.isFetching = true;
+    this.postsService.fetchPosts().subscribe(
+      posts => {
+        this.isFetching = false;
+        this.loadedPosts = posts;
+      },
+      error => {
+        this.isFetching = false;
+        this.error = error.message;
+      }
+    );
   }
 
   onCreatePost(postData: Post) {
@@ -31,14 +47,33 @@ export class AppComponent implements OnInit {
   onFetchPosts() {
     // Send Http request
     this.isFetching = true;
-    this.postsService.fetchPosts().subscribe(posts => {
-      this.isFetching = false;
-      this.loadedPosts = posts;
-    });
+    this.postsService.fetchPosts().subscribe(
+      posts => {
+        this.isFetching = false;
+        this.loadedPosts = posts;
+      },
+      error => {
+        this.isFetching = false;
+        this.error = error.message;
+        // this.error = error.error.error;
+        console.log(error);
+      }
+    );
   }
 
   onClearPosts() {
     // Send Http request
+    this.postsService.deletePosts().subscribe(() => {
+      this.loadedPosts = [];
+    });
+  }
+
+  onHandleError() {
+    this.error = null;
+  }
+
+  ngOnDestroy() {
+    this.errorSub.unsubscribe();
   }
 
 }
--- Angular/09_http/src/app/posts.service.ts
+++ Angular/09_http/src/app/posts.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { HttpClient, HttpHeaders, HttpParams, HttpEventType } from '@angular/common/http';
+import { map, catchError, tap } from 'rxjs/operators';
+import { Subject, throwError } from 'rxjs';
 
 import { Post } from './post.model';
 
 @Injectable({ providedIn: 'root' })
 export class PostsService {
+
+  error = new Subject<string>();  // for using when subscribing not in app.component.ts
 
   constructor(private http: HttpClient) { }
 
@@ -14,16 +17,36 @@ export class PostsService {
     this.http
       .post<{ name: string }>(
         'https://recipe-book-2b037.firebaseio.com/posts.json',
-        postData
+        postData,
+        {
+          observe: 'response'      // defaul is 'body'
+        }
       )
-      .subscribe(responseData => {    // subscribe not moved
-        console.log(responseData);
-      });
+      .subscribe(                // subscribe not moved (becouse we  don't care about answer)
+        responseData => {
+          console.log(responseData);
+        },
+        error => {
+          this.error.next(error.message);  // emitting error message
+
+        });
   }
 
   fetchPosts() {
+
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append('print', 'pretty');
+    searchParams = searchParams.append('custom', 'key');
+
     return this.http
-      .get<{ [key: string]: Post }>('https://recipe-book-2b037.firebaseio.com/posts.json')
+      .get<{ [key: string]: Post }>(
+        'https://recipe-book-2b037.firebaseio.com/posts.json',
+        {
+          headers: new HttpHeaders({ 'Custom-Header': 'Hello' }),
+          // params: new HttpParams().set('print', 'pretty')
+          params: searchParams
+        }
+      )
       // goal: convert object to array of posts:
       .pipe(
         map((responseData) => {
@@ -34,14 +57,37 @@ export class PostsService {
             }
           }
           return postsArray;
+        }),
+        catchError(errorResp => {
+          // Send to analytics server
+          return throwError(errorResp);
+
         })
       );
-    // .subscribe(posts => {    // subscribe  moved in tp the app.component.ts !!! therefore 'return' is added to enable this
+    // .subscribe(posts => {    // subscribe  moved in to the app.component.ts (becouse we  care about answer) !!! therefore 'return' is added to enable this
     //   console.log(posts);
     //   // this.isFetching = false;
     //   // this.loadedPosts = posts;
     // });
+  }
 
+  deletePosts() {
+    return this.http
+      .delete(
+        'https://recipe-book-2b037.firebaseio.com/posts.json',
+        {
+          observe: 'events'
+        }
+      ).pipe(tap(event => {      // tap is only for not disturbing subcribe()
+        console.log(event);
+        // checking if did I get response, etc. (by comparing code numbers)
+        if (event.type === HttpEventType.Sent) {
+          // ...
+        }
+        if (event.type === HttpEventType.Response) {
+          console.log(event.body);
+        }
+      }));
   }
 
 }
