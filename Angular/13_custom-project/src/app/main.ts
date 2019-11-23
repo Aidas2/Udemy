236b1d26afc8e08df5433ee8b90b1a0aab8980c0
Aidas <aidas.pupelis@gmail.com>
2019-11-23 20:41:57 +0200

    Angular FINISHEDgit add .!

---
+++ Angular/13_custom-project/src/app/main.ts
@@ -0,0 +1,6 @@
+import '../polyfills'; // shouldbe imported first
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
