49290df6a97360ed312496a02491ec6806c9ba6a
Aidas <aidas.pupelis@gmail.com>
2019-11-25 22:07:11 +0200

    Spring

--- Java Spring/hello-maven/src/main/java/academy/learnprogramming/HelloMaven.java
+++ Java Spring/hello-maven/src/main/java/academy/learnprogramming/HelloMaven.java
@@ -2,6 +2,6 @@ package academy.learnprogramming;
 
 public class HelloMaven {
     public static void main(String[] args) {
-        System.out.println("Hello Maven");
+        System.out.println("Hello Maven!");
     }
 }
