9a084cac67c9fa1004db0a5cc86a385c51bfc24d
Aidas Pupelis <aidas.pupelis@asseco.lt>
2019-11-26 17:42:42 +0200

    Spring log

--- Java Spring/hello-maven/src/main/java/academy/learnprogramming/HelloMaven.java
+++ Java Spring/hello-maven/src/main/java/academy/learnprogramming/HelloMaven.java
@@ -1,7 +1,15 @@
 package academy.learnprogramming;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HelloMaven {
+
+    private final static Logger log = LoggerFactory.getLogger(HelloMaven.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello Maven!");
+//        System.out.println("Hello Maven!");
+        log.info("Hello Info");
+        log.debug("Hello Debug");
     }
 }
