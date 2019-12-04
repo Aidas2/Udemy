f0086e75af9d89d0e040890f6ff8cb0308680e18
Aidas <aidas.pupelis@gmail.com>
2019-12-04 21:06:06 +0200

    Spring

--- Java Spring/todolist/src/main/java/academy/learnprogramming/controller/DemoController.java
+++ Java Spring/todolist/src/main/java/academy/learnprogramming/controller/DemoController.java
@@ -1,16 +1,29 @@
 package academy.learnprogramming.controller;
 
+import academy.learnprogramming.service.DemoService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
 @Controller
 public class DemoController {
 
+    // == fields ==
+    private final DemoService demoService;
+
+    // == constructors ==
+    @Autowired
+    public DemoController(DemoService demoService) {
+        this.demoService = demoService;
+    }
+
+    // == request methods ==
     // http://localhost:8080/todo-list/hello
     @ResponseBody
     @GetMapping("/hello")
@@ -19,18 +32,24 @@ public class DemoController {
     }
 
     // http://localhost:8080/todo-list/welcome
+    // http://localhost:8080/todo-list/welcome?user=Tim
+    // http://localhost:8080/todo-list/welcome?user=Tim&age=45
     @GetMapping("welcome")
-    public String welcome(Model model) {
-        model.addAttribute("user", "Tim");
+    public String welcome(@RequestParam String user, @RequestParam int age, Model model) {
+        model.addAttribute("helloMessage", demoService.getHelloMessage(user));
+        model.addAttribute("age", age);
         log.info("model = {}", model);
+
         // prefix + name + suffix
         // /WEB-INF/view/welcome.jsp
         return "welcome";   // this name must match file welcome.jsp name
     }
 
+    // == model attributes ==
     @ModelAttribute("welcomeMessage")   // name must match attribute in jsp file
     public String welcomeMessage() {
         log.info("welcomeMessage() called");
-        return "Welcome to this Demo application.";
+        return demoService.getWelcomeMessage();
     }
+
 }
---
+++ Java Spring/todolist/src/main/java/academy/learnprogramming/model/TodoData.java
@@ -0,0 +1,77 @@
+package academy.learnprogramming.model;
+
+import lombok.NonNull;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.ListIterator;
+
+public class TodoData {
+
+    // == fields ==
+    private static int idValue = 1;
+
+    private final List<TodoItem> items = new ArrayList<>();
+
+    // == constructors ==
+    public TodoData() {
+
+        // add some dummy data, using the addItem method(see below) so it sets the id field
+        addItem(new TodoItem("first", "first details", LocalDate.now()));
+        addItem(new TodoItem("second", "second details", LocalDate.now()));
+        addItem(new TodoItem("third", "third details", LocalDate.now()));
+    }
+
+    // == public methods ==
+    public List<TodoItem> getItems() {
+        return Collections.unmodifiableList(items);
+    }
+
+    public void addItem(@NonNull TodoItem toAdd) {
+
+//        if(toAdd == null) { // replaced with lombok annotation @NonNull
+//            throw new NullPointerException("toAdd is a required parameter");
+//        }
+
+        toAdd.setId(idValue);
+        items.add(toAdd);
+        idValue++;
+    }
+
+    public void removeItem(int id) {
+        ListIterator<TodoItem> itemIterator = items.listIterator();
+
+        while(itemIterator.hasNext()) {
+            TodoItem item = itemIterator.next();
+
+            if(item.getId() == id) {
+                itemIterator.remove();
+                break;
+            }
+        }
+    }
+
+    public TodoItem getItem(int id) {
+        for(TodoItem item : items) {
+            if(item.getId() == id) {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    public void updateItem(@NonNull TodoItem toUpdate) {
+        ListIterator<TodoItem> itemIterator = items.listIterator();
+
+        while(itemIterator.hasNext()) {
+            TodoItem item = itemIterator.next();
+
+            if(item.equals(toUpdate)) { // .equals is from "equals and hashcode" (lombok)
+                itemIterator.set(toUpdate); // .set is from iterator
+                break;
+            }
+        }
+    }
+}
