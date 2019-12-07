637d5bd20752e314cadb791e2c0538eb6a74b4e2
Aidas <aidas.pupelis@gmail.com>
2019-12-07 22:27:38 +0200

    Spring

--- Java Spring/todolist/src/main/java/academy/learnprogramming/controller/TodoItemController.java
+++ Java Spring/todolist/src/main/java/academy/learnprogramming/controller/TodoItemController.java
@@ -10,9 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
@@ -42,8 +40,15 @@ public class TodoItemController {
     }
 
     @GetMapping(Mappings.ADD_ITEM)
-    public String addEditItem(Model model) {
-        TodoItem todoItem = new TodoItem("", "", LocalDate.now());
+    public String addEditItem(@RequestParam(required = false, defaultValue = "-1") int id,
+                              Model model) {
+        log.info("editing id = {}", id);
+        TodoItem todoItem = todoItemsService.getItem(id);
+
+        if(todoItem == null) {
+            todoItem = new TodoItem("", "", LocalDate.now());
+        }
+
         model.addAttribute(AttributeNames.TODO_ITEM, todoItem);
         return ViewNames.ADD_ITEM;
     }
@@ -52,7 +57,29 @@ public class TodoItemController {
     @PostMapping(Mappings.ADD_ITEM)
     public String processItem(@ModelAttribute(AttributeNames.TODO_ITEM) TodoItem todoItem) {
         log.info("todoItem from form = {}", todoItem);
-        todoItemsService.addItem(todoItem);
+
+        if(todoItem.getId() == 0) {
+            todoItemsService.addItem(todoItem);
+        } else {
+            todoItemsService.updateItem(todoItem);
+        }
+
         return "redirect:/" + Mappings.ITEMS;
     }
+
+    @GetMapping(Mappings.DELETE_ITEM)
+    public String deleteItem(@RequestParam int id) {
+        log.info("Deleting item with id={}", id);
+        todoItemsService.removeItem(id);
+        return "redirect:/" + Mappings.ITEMS;
+    }
+
+    @GetMapping(Mappings.VIEW_ITEM)
+    public String viewItem(@RequestParam int id, Model model) {
+        log.info("Viewing item with id={}", id);
+        TodoItem todoItem = todoItemsService.getItem(id);
+        model.addAttribute(AttributeNames.TODO_ITEM, todoItem);
+        return ViewNames.VIEW_ITEM;
+    }
+
 }
--- Java Spring/todolist/src/main/webapp/WEB-INF/view/items_list.jsp
+++ Java Spring/todolist/src/main/webapp/WEB-INF/view/items_list.jsp
@@ -16,11 +16,21 @@
             <tr>
                 <th>Title</th>
                 <th>Deadline</th>
+                <th>View</th>
+                <th>Edit</th>
                 <th>Delete</th>
 
             </tr>
 
             <c:forEach var="item" items="${todoData.items}">
+
+                <c:url var="viewUrl" value="${Mappings.VIEW_ITEM}">
+                    <c:param name="id" value="${item.id}"/>
+                </c:url>
+
+                <c:url var="editUrl" value="${Mappings.ADD_ITEM}">
+                    <c:param name="id" value="${item.id}"/>
+                </c:url>
 
                 <c:url var="deleteUrl" value="${Mappings.DELETE_ITEM}">
                     <c:param name="id" value="${item.id}"/>
@@ -29,8 +39,10 @@
                 <tr>
                     <td><c:out value="${item.title}"/></td>
                     <td><c:out value="${item.deadline}"/></td>
+                    <td><a href="${viewUrl}">View</a></td>
+                    <td><a href="${editUrl}">Edit</a></td>
                     <td><a href="${deleteUrl}">Delete</a></td>
-                <tr/>
+                </tr>
             </c:forEach>
         </table>
     </div>
