52091792487988730e26d5ce404d5cb02fd1da41
Aidas Pupelis <aidas.pupelis@asseco.lt>
2019-12-16 17:36:59 +0200

    Spring

---
+++ Java Spring/guess-the-number-game/web/src/main/java/academy/learnprogramming/util/GameMappings.java
@@ -0,0 +1,11 @@
+package academy.learnprogramming.util;
+
+public final class GameMappings {
+
+    // == constants ==
+    public static final String PLAY = "play";
+    public static final String REDIRECT_PLAY = "redirect:/" + PLAY;
+
+    // == constructor ==
+    private GameMappings() {}
+}
--- Java Spring/guess-the-number-game/web/src/main/java/academy/learnprogramming/util/ViewNames.java
+++ Java Spring/guess-the-number-game/web/src/main/java/academy/learnprogramming/util/ViewNames.java
@@ -4,6 +4,8 @@ public final class ViewNames {
 
     // == constants ==
     public static final String HOME = "home"; // string must match file name in resources/templates/home.html
+    public static final String PLAY = "play";
+    public static final String GAME_OVER = "game-over";
 
     // == constructors ==
     private ViewNames() {}
