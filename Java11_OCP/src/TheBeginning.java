d435cb12cb1cfb00aef58ab87a61144ce76fe345
Aidas Pupelis <aidas.pupelis@asseco.lt>
2020-03-02 17:24:16 +0200

    java OCP

---
+++ Java11_OCP/src/TheBeginning.java
@@ -0,0 +1,10 @@
+/*
+Learn Programming Academy's Java 1Z0-815 Certification Exam Course
+Section 2: Create an executable Java program
+*/
+
+public class TheBeginning {
+    public static void main(String[] args) {
+        System.out.println("This executes");
+    }
+}
