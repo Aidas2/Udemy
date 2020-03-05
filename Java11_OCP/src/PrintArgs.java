acb419a6a3aae48c3b27998f9d0d0fc599bbc8c0
Aidas Pupelis <aidas.pupelis@asseco.lt>
2020-03-05 17:17:19 +0200

    psvm

---
+++ Java11_OCP/src/PrintArgs.java
@@ -0,0 +1,24 @@
+/*
+Learn Programming Academy's Java 1Z0-815 Certification Exam Course
+Section 2: Create an executable Java program
+*/
+
+public class PrintArgs {
+
+    public static void main(String[] args) {
+
+        System.out.println("Printing some arguments in this code: ");
+
+        // Loop through arguments passed and print them to standard output
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Argument " + (i + 1) + ": " + args[i]);
+        }
+    }
+}
+
+// C:\Udemy_etc\Java11_OCP>java -cp out\production\Java11_OCP PrintArgs
+// C:\Udemy_etc\Java11_OCP>java -classpath out\production\Java11_OCP PrintArgs Test1 Test2 Test3
+
+// C:\Udemy_etc\Java11_OCP>java --class-path out\production\Java11_OCP PrintArgs One1,Two2,Three3 "Two Three" 'Four Five'
+
+
--- Java11_OCP/src/TheBeginning.java
+++ Java11_OCP/src/TheBeginning.java
@@ -3,8 +3,13 @@ Learn Programming Academy's Java 1Z0-815 Certification Exam Course
 Section 2: Create an executable Java program
 */
 
-public class TheBeginning {
+class TheStart {
     public static void main(String[] args) {
         System.out.println("This executes");
     }
+}
+
+// Demonstrate multiple classes in one source file
+class Next {
+
 }
