378a7d7085f7a3b736174df2e7094a95f46f44b5
Aidas Pupelis <aidas.pupelis@gmail.com>
2020-04-14 21:31:14 +0300

    Spring MVC FormValidation

---
+++ Java_Spring/OLD_Spring4/MVC_part3/spring-mvc-form-validation-demo/src/main/java/com/timbuchalka/springdemo/controller/test/RequestMappingAndParamDemoController.java
@@ -0,0 +1,119 @@
+package com.timbuchalka.springdemo.controller.test;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping(value="/requestMappingAndParamDemo")
+@Controller
+public class RequestMappingAndParamDemoController {
+	
+	private static Logger LOGGER = LoggerFactory.getLogger(RequestMappingAndParamDemoController.class);
+
+	@RequestMapping(value = "/home")
+	public String home() {
+		return "requestMappingAndParamHome";
+	}
+
+	// test 1: Testing @RequestParam without explicit attributes
+	@RequestMapping(value = "/test1")
+	public String requestMappingAndParamTest1(@RequestParam("orgname") String orgName, Model model) {
+		model.addAttribute("orgname", orgName);
+		model.addAttribute("testserial", "test1");
+		return "requestMappingAndParamResults";	//this is jsp file name
+	}
+
+	// test 2: Testing @RequestMapping 'method' attribute
+	@RequestMapping(value = "/test2", method = RequestMethod.GET)
+	public String requestMappingAndParamTest2(@RequestParam("orgname") String orgName, Model model) {
+		model.addAttribute("orgname", orgName);
+		model.addAttribute("testserial", "test2");
+		return "requestMappingAndParamResults";
+	}
+
+	// test 3: Testing @RequestMapping fall back feature
+	@RequestMapping(value = "*", method = RequestMethod.GET)
+	// @RequestMapping(value="*", method = {RequestMethod.GET, RequestMethod.POST})
+	public String requestMappingAndParamTest3() {
+		return "fallback";
+	}
+
+	// test 4: Testing @RequestParam 'defaultValue'attribute
+	@RequestMapping(value = "/test4")
+	public String requestMappingAndParamTest4(
+			@RequestParam(value = "orgname", defaultValue = "Anonymous Organization") String orgName, Model model) {
+		model.addAttribute("orgname", orgName);
+		model.addAttribute("testserial", "test4");
+		return "requestMappingAndParamResults";
+	}
+
+	// test 5: Testing @RequestParam without 'name'or 'value' attributes
+	@RequestMapping(value = "/test5", method = RequestMethod.GET)
+	public String requestMappingAndParamTest5(@RequestParam String orgname, Model model) {
+		model.addAttribute("orgname", orgname);
+		model.addAttribute("testserial", "test5");
+		return "requestMappingAndParamResults";
+	}
+
+	// test 6, subtest 1: Testing removal of @RequestMapping ambiguity with the
+	// same base URI but  with different a different parameter
+	@RequestMapping(value = "/test6", params = "orgname")
+	public String requestMappingAndParamTest6Subtest1(@RequestParam String orgname, Model model) {
+		model.addAttribute("orgname", orgname);
+		model.addAttribute("testserial", "test6-subtest1");
+		return "requestMappingAndParamResults2";
+	}	
+//	http://localhost:8080/spring-mvc-request-mapping-param-demo/requestMappingAndParamDemo/test6?orgname=Microsoft
+
+	// test 6, subtest 2: Testing removal of @RequestMapping ambiguity with the
+	// same base URI but with different a different parameter
+	@RequestMapping(value = "/test6", params = "empcount")
+	public String requestMappingAndParamTest6Subtest2(@RequestParam String empcount, Model model) {
+		model.addAttribute("orgname", empcount);
+		model.addAttribute("testserial", "test6-subtest2");
+		return "requestMappingAndParamResults2";
+	}
+//	http://localhost:8080/spring-mvc-request-mapping-param-demo/requestMappingAndParamDemo/test6?empcount=56
+
+	// test 6, subtest 3: Testing removal of @RequestMapping with multiple
+	// request params
+	@RequestMapping(value = "/test6/subtest3", method = RequestMethod.GET, params = { "orgname", "empcount" })
+	public String requestMappingAndParamTest6Subtest3(@RequestParam String orgname, @RequestParam String empcount,
+			Model model) {
+		model.addAttribute("orgname", orgname);
+		model.addAttribute("empCount", empcount);
+		model.addAttribute("testserial", "test6-subtest3");
+		return "requestMappingAndParamResults2";
+	}
+//	http://localhost:8080/spring-mvc-request-mapping-param-demo/requestMappingAndParamDemo/test6/subtest3?orgname=IBM&empcount=56
+	
+	// test 6, subtest 4: Testing with multiple request params and @RequestParam with single param
+	@RequestMapping(value = "/test6/subtest4", method = RequestMethod.GET, params = { "orgname", "empcount" })
+	public String requestMappingAndParamTest6Subtest4(@RequestParam String orgname, Model model) {
+		model.addAttribute("orgname", orgname);
+		model.addAttribute("testserial", "test6-subtest4");
+		return "requestMappingAndParamResults2";
+	}
+//	http://localhost:8080/spring-mvc-request-mapping-param-demo/requestMappingAndParamDemo/test6/subtest4?orgname=Honda&empcount=
+
+	// test 7 & 8: Testing @RequestParam with multiple request URI's
+	@RequestMapping(value = {"/test7", "/test8"}, method = RequestMethod.GET)
+	public String requestMappingAndParamTest7and8(@RequestParam String orgname, Model model, HttpServletRequest request) {
+		model.addAttribute("orgname", orgname);
+		LOGGER.info(request.getRequestURL().toString());
+		if(request.getRequestURL().toString().contains("test7")) {
+			model.addAttribute("testserial", "test7");
+		} else {
+			model.addAttribute("testserial", "test8");
+		}
+		return "requestMappingAndParamResults2";
+	}
+//	http://localhost:8080/spring-mvc-request-mapping-param-demo/requestMappingAndParamDemo/test7?orgname=Canon
+//	http://localhost:8080/spring-mvc-request-mapping-param-demo/requestMappingAndParamDemo/test8?orgname=Canon
+}
---
+++ Java_Spring/OLD_Spring4/MVC_part3/spring-mvc-form-validation-demo/src/main/java/com/timbuchalka/springdemo/validation/test/AgeConstraintValidator.java
@@ -0,0 +1,31 @@
+package com.timbuchalka.springdemo.validation.test;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class AgeConstraintValidator implements ConstraintValidator<AgeConstraint, Integer> {
+	
+	private int lower;
+	private int upper;
+	
+	@Override
+	public void initialize(AgeConstraint constraintAnnotation) {
+		lower = constraintAnnotation.lower();
+		upper = constraintAnnotation.upper();
+	}
+	
+	@Override
+	public boolean isValid(Integer age, ConstraintValidatorContext context) {
+		if(age == null) {
+			return false;
+		}
+		if(age < lower || age > upper) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	
+
+}
