c0bbd571c82b026162b769d06dc058f50d92c5bb
AidasP <aidas.pupelis@gmail.com>
2021-04-03 15:37:10 +0300

    reordered2

--- Java Collections Framework + Generics, Lambdas & Stream API/learnit_java_core-starter/src/com/learnit/List/ListPerformanceEvaluation.java
+++ Java Collections Framework + Generics, Lambdas & Stream API/learnit_java_core-starter/src/com/learnit/List/ListPerformanceEvaluation.java
@@ -14,32 +14,44 @@ public class ListPerformanceEvaluation {
 
     public static void addElementsToBeginning(List<Integer> list,
                                               int numberOfElementsToAdd) {
-//		<write your code here>
+        for (int i = 0; i < numberOfElementsToAdd; i++) {
+            list.add(0, Integer.MAX_VALUE);
+        }
     }
 
     public static void addElementsToMiddle(List<Integer> list,
                                            int numberOfElementsToAdd) {
-//			<write your code here>
+        for (int i = 0; i < numberOfElementsToAdd; i++) {
+            list.add(list.size() / 2, Integer.MAX_VALUE);
+        }
 
     }
 
     public static void addElementsToEnd(List<Integer> list, int numberOfElementsToAdd) {
-//			<write your code here>
+        for (int i = 0; i < numberOfElementsToAdd; i++) {
+            list.add(Integer.MAX_VALUE);
+        }
     }
 
     public static void removeElementsFromBeginning(List<Integer> list,
                                                    int numberOfElementsToRemove) {
-//		<write your code here>
+        for (int i = 0; i < numberOfElementsToRemove; i++) {
+            list.remove(0);
+        }
     }
 
     public static void removeElementsFromMiddle(List<Integer> list,
                                                 int numberOfElementsToRemove) {
-//			<write your code here>
+        for (int i = 0; i < numberOfElementsToRemove; i++) {
+            list.remove(list.size() / 2);
+        }
     }
 
     public static void removeElementsFromEnd(List<Integer> list,
                                              int numberOfElementsToRemove) {
-//			<write your code here>
+        for (int i = 0; i < numberOfElementsToRemove; i++) {
+            list.remove(list.size() - 1);
+        }
     }
 
     public static void main(String[] args) {
