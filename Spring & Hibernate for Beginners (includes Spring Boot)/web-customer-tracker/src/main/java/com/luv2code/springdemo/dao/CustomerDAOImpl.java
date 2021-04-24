07ee36c32fd39de1533e3fa75a6250d81e7adadb
AidasP <aidas.pupelis@gmail.com>
2021-04-24 10:34:22 +0300

    search/sort

--- Spring & Hibernate for Beginners (includes Spring Boot)/web-customer-tracker/src/main/java/com/luv2code/springdemo/dao/CustomerDAOImpl.java
+++ Spring & Hibernate for Beginners (includes Spring Boot)/web-customer-tracker/src/main/java/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.util.SortUtils;
 
 @Repository
 public class CustomerDAOImpl implements CustomerDAO {
@@ -18,16 +19,52 @@ public class CustomerDAOImpl implements CustomerDAO {
 	@Autowired
 	private SessionFactory sessionFactory;
 			
-	@Override
-	@Transactional
-	public List<Customer> getCustomers() {
+//	@Override 		// without sorting
+//	public List<Customer> getCustomers() {
+//		
+//		// get the current hibernate session
+//		Session currentSession = sessionFactory.getCurrentSession();
+//				
+//		// create a query  ... sort by last name
+//		Query<Customer> theQuery = 
+//				currentSession.createQuery("from Customer order by lastName",
+//											Customer.class);
+//		
+//		// execute query and get result list
+//		List<Customer> customers = theQuery.getResultList();
+//				
+//		// return the results		
+//		return customers;
+//	}
+	
+	@Override		// with sorting
+	public List<Customer> getCustomers(int theSortField) {
 		
 		// get the current hibernate session
 		Session currentSession = sessionFactory.getCurrentSession();
 				
-		// create a query
+		// determine sort field
+		String theFieldName = null;
+		
+		switch (theSortField) {
+			case SortUtils.FIRST_NAME: 
+				theFieldName = "firstName";
+				break;
+			case SortUtils.LAST_NAME:
+				theFieldName = "lastName";
+				break;
+			case SortUtils.EMAIL:
+				theFieldName = "email";
+				break;
+			default:
+				// if nothing matches the default to sort by lastName
+				theFieldName = "lastName";
+		}
+		
+		// create a query  
+		String queryString = "from Customer order by " + theFieldName;
 		Query<Customer> theQuery = 
-				currentSession.createQuery("from Customer", Customer.class);
+				currentSession.createQuery(queryString, Customer.class);
 		
 		// execute query and get result list
 		List<Customer> customers = theQuery.getResultList();
@@ -36,7 +73,80 @@ public class CustomerDAOImpl implements CustomerDAO {
 		return customers;
 	}
 
+	@Override
+	public void saveCustomer(Customer theCustomer) {
+
+		// get current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// save/update the customer ... finally LOL
+		currentSession.saveOrUpdate(theCustomer);
+		
+	}
+
+	@Override
+	public Customer getCustomer(int theId) {
+
+		// get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// now retrieve/read from database using the primary key
+		Customer theCustomer = currentSession.get(Customer.class, theId);
+		
+		return theCustomer;
+	}
+
+	@Override
+	public void deleteCustomer(int theId) {
+
+		// get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// delete object with primary key
+		Query theQuery = 
+				currentSession.createQuery("delete from Customer where id=:customerId");
+		theQuery.setParameter("customerId", theId);
+		
+		theQuery.executeUpdate();		
+	}
+
+	@Override
+	public List<Customer> searchCustomers(String theSearchName) {
+
+		// get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		Query theQuery = null;
+		
+		//
+		// only search by name if theSearchName is not empty
+		//
+		if (theSearchName != null && theSearchName.trim().length() > 0) {
+
+			// search for firstName or lastName ... case insensitive
+			theQuery =currentSession.createQuery("from Customer where lower(firstName) like :theName or lower(lastName) like :theName", Customer.class);
+			theQuery.setParameter("theName", "%" + theSearchName.toLowerCase() + "%");
+
+		}
+		else {
+			// theSearchName is empty ... so just get all customers
+			theQuery =currentSession.createQuery("from Customer", Customer.class);			
+		}
+		
+		// execute query and get result list
+		List<Customer> customers = theQuery.getResultList();
+				
+		// return the results		
+		return customers;
+		
+	}
+
 }
+
+
+
+
+
 
 
 
--- Spring & Hibernate for Beginners (includes Spring Boot)/web-customer-tracker/src/main/webapp/WEB-INF/view/list-customers.jsp
+++ Spring & Hibernate for Beginners (includes Spring Boot)/web-customer-tracker/src/main/webapp/WEB-INF/view/list-customers.jsp
@@ -1,4 +1,6 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ page import="com.luv2code.springdemo.util.SortUtils" %>
 
 <!DOCTYPE html>
 
@@ -27,22 +29,73 @@
 	
 		<div id="content">
 		
+			<!-- put new button: Add Customer -->
+		
+			<input type="button" value="Add Customer"
+				   onclick="window.location.href='showFormForAdd'; return false;"
+				   class="add-button"
+			/>
+			
+			<!--  add a search box -->
+			<form:form action="search" method="GET">
+				Search customer: <input type="text" name="theSearchName" />
+				
+				<input type="submit" value="Search" class="add-button" />
+			</form:form>
+		
 			<!--  add our html table here -->
 		
 			<table>
+				<!-- setup header links for sorting -->
+
+				<!-- construct a sort link for first name -->
+				<c:url var="sortLinkFirstName" value="/customer/list">
+					<c:param name="sort" value="<%= Integer.toString(SortUtils.FIRST_NAME) %>" />
+				</c:url>					
+
+				<!-- construct a sort link for last name -->
+				<c:url var="sortLinkLastName" value="/customer/list">
+					<c:param name="sort" value="<%= Integer.toString(SortUtils.LAST_NAME) %>" />
+				</c:url>					
+
+				<!-- construct a sort link for email -->
+				<c:url var="sortLinkEmail" value="/customer/list">
+					<c:param name="sort" value="<%= Integer.toString(SortUtils.EMAIL) %>" />
+				</c:url>					
+
 				<tr>
-					<th>First Name</th>
-					<th>Last Name</th>
-					<th>Email</th>
+					<th><a href="${sortLinkFirstName}">First Name</a></th>
+					<th><a href="${sortLinkLastName}">Last Name</a></th>
+					<th><a href="${sortLinkEmail}">Email</a></th>
+					<th>Action</th>
 				</tr>
 				
 				<!-- loop over and print our customers -->
 				<c:forEach var="tempCustomer" items="${customers}">
 				
+					<!-- construct an "update" link with customer id -->
+					<c:url var="updateLink" value="/customer/showFormForUpdate">
+						<c:param name="customerId" value="${tempCustomer.id}" />
+					</c:url>					
+
+					<!-- construct an "delete" link with customer id -->
+					<c:url var="deleteLink" value="/customer/delete">
+						<c:param name="customerId" value="${tempCustomer.id}" />
+					</c:url>					
+					
 					<tr>
 						<td> ${tempCustomer.firstName} </td>
 						<td> ${tempCustomer.lastName} </td>
 						<td> ${tempCustomer.email} </td>
+						
+						<td>
+							<!-- display the update link -->
+							<a href="${updateLink}">Update</a>
+							|
+							<a href="${deleteLink}"
+							   onclick="if (!(confirm('Are you sure you want to delete this customer?'))) return false">Delete</a>
+						</td>
+						
 					</tr>
 				
 				</c:forEach>
