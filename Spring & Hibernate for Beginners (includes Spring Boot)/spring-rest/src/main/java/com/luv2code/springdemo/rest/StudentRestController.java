9370c5e83e6f859d10f8bf163c1789db4044af19
AidasP <aidas.pupelis@gmail.com>
2021-05-11 18:06:11 +0300

    exception

--- Spring & Hibernate for Beginners (includes Spring Boot)/spring-rest/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ Spring & Hibernate for Beginners (includes Spring Boot)/spring-rest/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -53,14 +53,38 @@ public class StudentRestController {
 		
 		// check the studentId against list size
 		
-//		if ( (studentId >= theStudents.size()) || (studentId < 0) ) {
-//			throw new StudentNotFoundException("Student id not found - " + studentId);
-//		}
+		if ( (studentId >= theStudents.size()) || (studentId < 0) ) {
+			throw new StudentNotFoundException("Student id not found - " + studentId);
+		}
 			
 		return theStudents.get(studentId);
 		
 	}	
-	
+	/*  moved to separate class StudentRestExceptionHandler.java (as AOP with @ControllerAdvice)
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
+		
+		StudentErrorResponse error = new StudentErrorResponse();
+		
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+		
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+	 }
+
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
+		
+		StudentErrorResponse error = new StudentErrorResponse();
+		
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+		
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+	}
+	*/
 }
 
 
