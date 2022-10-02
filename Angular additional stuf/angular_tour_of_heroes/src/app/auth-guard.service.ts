222c42b9535a6247ee41512bca0b48c4ff2034de
AidasP <aidas.pupelis@gmail.com>
2022-10-02 12:42:00 +0300

    moved again

--- Angular additional stuf/angular_tour_of_heroes/src/app/auth-guard.service.ts
+++
@@ -1,9 +0,0 @@
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { Observable} from 'rxjs/Observable';
-
-export class AuthGuard implements CanActivate {
-  canActivate(route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promisse<boolean> | boolean {
-
-    }
-}
