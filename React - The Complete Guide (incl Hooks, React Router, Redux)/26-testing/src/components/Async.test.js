417a3a31be106e1ce06c3e39f6648fe886b85b34
AidasP <aidas.pupelis@gmail.com>
2022-10-04 21:36:20 +0300

    testing

--- React - The Complete Guide (incl Hooks, React Router, Redux)/26-testing/src/components/Async.test.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/26-testing/src/components/Async.test.js
@@ -3,7 +3,14 @@ import Async from './Async';
 
 describe('Async component', () => {
   test('renders posts if request succeeds', async () => {
-    render(<Async />)
+	  
+    window.fetch = jest.fn(); // for mocking
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: 'p1', title: 'First post' }],
+    }); // for mocking
+	// without those two lines real server/database will be used (no good)
+	
+    render(<Async />);
 
     const listItemElements = await screen.findAllByRole('listitem');
     expect(listItemElements).not.toHaveLength(0);
